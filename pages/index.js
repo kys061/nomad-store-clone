052e60fb362ef56e67287e5136c1e38fbf1a05e5
yskang <kys061@gmail.com>
2018-12-05 22:17:57 +0900

    first commit

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import Link from "next/link";
+import withLayout from "../lib/withLayout";
+
+const Index = () => (
+    <div>
+        <Head>
+            <title>Home | Nomad Store</title>
+        </Head>
+        <h1>Hello from the index</h1>{" "}
+    </div>
+);
+
+export default withLayout(Index);
