bbf35af7c4bc2bdf5da169e5bdc196d1c8931e2c
yskang <kys061@gmail.com>
2018-12-05 22:48:59 +0900

    pretty pages #2

--- server.js
+++ server.js
@@ -9,9 +9,17 @@ app
     .prepare()
     .then(() => {
         const server = express();
+        
+        server.get("/post/:title", (req, res) => {
+            const actualPage = "/post";
+            const queryParams = { title: req.params.title };
+            app.render(req, res, actualPage, queryParams);
+        });
+        
         server.get("*", (req, res) => {
         return handle(req, res);
         });
+        
         server.listen(3000, err => {
         if (err) throw err;
         console.log("> Ready on http://localhost:3000");
